b1318b38623c556801a7628f483db52b213b3200
ssenmosip <srijoni@mosip.io>
2019-11-12 23:02:12 +0530

    readthedocs integration

--- conf.py
+++ conf.py
@@ -38,7 +38,8 @@ extensions = ['sphinx.ext.doctest',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['rtdtemplates']
