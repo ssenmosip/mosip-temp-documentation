bccd9a38e70589a09dada15c8ede4f5d1077686a
ssenmosip <srijoni@mosip.io>
2019-11-13 20:57:29 +0530

    trying docusaurus

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'MOSIP Documentation', // Title for your website.
   tagline: 'A website for testing MOSIP Documentation',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://ssenmosip.github.io', // Your website URL
+  baseUrl: '/mosip-temp-documentation', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'mosip-test-docscd ',
-  organizationName: 'mosip',
+  projectName: 'mosip-temp-documentation',
+  organizationName: 'ssenmosip',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
