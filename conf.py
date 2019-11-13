66807a2fc223d4d66c403a36fde63b3827f8120f
ssenmosip <srijoni@mosip.io>
2019-11-13 18:21:51 +0530

    removed readthedocs

--- conf.py
+++ conf.py
@@ -84,7 +84,7 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-html_theme = "sphinx_rtd_theme"
+
 
 html_theme_options = {
     'canonical_url': '',
@@ -108,7 +108,7 @@ html_theme_options = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
